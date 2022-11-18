144eec5e1d70991ca8df6d7cc616823494f3c4dd
Kyoya2 <emenyrom@protonmail.com>
2022-11-18 16:58:08 +0200

    Prettier setup script

--- setup.bat
+++ setup.bat
@@ -8,11 +8,13 @@ echo Choose a setup type
 echo [1] - Replace `user.bind` with the new `user.bind`. Use this if you didn't clone the repository and don't want updates on pull.
 echo [2] - Replace `user.bind` with a hardlink to the new `user.bind`. Use this if you cloned the repository and want the file to be automatically updated on pull.
 choice /c 12
+cls
 set setup_type=%errorlevel%
 
 REM Try to delete the current keybind file if it exists
 if exist %BINDS_FILE_DEST% (
     choice /c yn /m "Are you sure you want to delete the current keybindings file?"
+    cls
     if !errorlevel!==2 goto EOF
     del %BINDS_FILE_DEST% /q /f
 )
